c91198493b2d17ccfc9f2354a14a3b2c25226ca1
DavidTorres-sys <david.torresg@udea.edu.co>
2024-11-18 11:53:46 -0500

    Add utility functions, components, and update authentication integration; introduce sidebar items and build workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { SidebarProvider, SidebarTrigger } from "@/components/molecules/SidebarSection";
+import { AppSidebar } from "@/components/organisms/sidebar";
+import { sign } from "crypto";
+import { useSession, signIn } from "next-auth/react";
+
+export default function Home({ children }: { children: React.ReactNode }) {
+  // const { data: session, status } = useSession();
+  // if (!session) {
+  //   signIn('auth0');
+  // }
   return (
     <div>
-      <h1>Hello Robinsón Coronado</h1>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
     </div>
   );
 }
