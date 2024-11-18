da6fd7dd555ac7179b430365fe3869a9cbde0c54
Ferney <ferney.montoyag@gmail.com>
2024-11-18 11:57:13 -0500

    start with the login feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,21 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client";
+import safeJsonStringify from "safe-json-stringify";
+import { useSession, signIn } from "next-auth/react";
+
+export async function getServerSideProps() {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  console.log(safeJsonStringify(users));
+  return { props: { users: safeJsonStringify(users) } };
+}
+
+export default function Home({ users }: any) {
+  const { data: session } = useSession();
+
+  if (!session) {
+    signIn('auth0');
+  }
+  console.log(users);
   return (
     <div>
       <h1>Hello Robinsón Coronado</h1>
