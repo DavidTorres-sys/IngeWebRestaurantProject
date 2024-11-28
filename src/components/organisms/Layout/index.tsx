7c245327e5f122f0dcca54ef79faa879a12f55f3
DavidTorres-sys <david.torresg@udea.edu.co>
2024-11-28 11:30:20 -0500

    Add Apollo Client integration, create user mutations, and implement user form with validation

---
+++ src/components/organisms/Layout/index.tsx
@@ -0,0 +1,31 @@
+import {
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"; 
+import { AppSidebar } from "@/components/organisms/Sidebar";
+import { useEffect } from "react";
+import { useSession, signIn } from "next-auth/react";
+
+export default function Home({ children }: any) {
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    if (!session && status !== "loading") {
+      signIn("auth0");
+    }
+  }, [session, status]);
+
+  const userName = session?.user.name || "Guest";
+  const UserImage = session?.user.image || null;
+
+  return (
+    <div>
+      <SidebarProvider>
+        <AppSidebar role="admin" image={UserImage} name={userName} />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </div>
+  );
+}
